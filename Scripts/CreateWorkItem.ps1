7019af54241247cb3cc9aaf4882867f5686229b4
Anji6789 <59438036+Anji6789@users.noreply.github.com>
2022-02-03 16:01:43 +0530

    Update CreateWorkItem.ps1

--- Scripts/CreateWorkItem.ps1
+++ Scripts/CreateWorkItem.ps1
@@ -12,7 +12,7 @@ Invoke-RestMethod -Uri $uriAccount -Method get -Headers $AzureDevOpsAuthenicatio
 
 #Create a work item
 
-$WorkItemType = "epic"
+$WorkItemType = "task"
 $WorkItemTitle = "Test from Powershell"
 $WorkItemDescription = "Test Description"
 $ProjectName = "apicall";
